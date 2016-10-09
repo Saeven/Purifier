54570be217db2605f835eba5b9b7295fdc0f565a
Alexandre Lemaire <saeven@saeven.net>
2016-10-09 00:23:05 -0400

    Trying to add the module to the filter chain as CleanHtml as a test.

--- config/module.config.php
+++ config/module.config.php
@@ -20,7 +20,6 @@ return [
     'service_manager' => [
         'factories' => [
             'HTMLPurifierEngine' => HtmlPurifierFactory::class,
-            'CleanHtml' => PurifierFilterFactory::class,
         ],
     ],
 
